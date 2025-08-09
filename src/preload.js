2ea83c7234b60c5564f2c8e49f3255dd9ec68d6d
ivanvihe <ivan.vihe@gmail.com>
2025-08-09 20:27:50 +0200

    delete all

--- src/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    on: (channel, callback) => ipcRenderer.on(channel, (_event, data) => callback(data))
-});
--- src/renderer/controls.js
+++
@@ -1,8 +0,0 @@
-import { Pane } from 'tweakpane';
-
-const pane = new Pane({ container: document.getElementById('controls') });
-const params = { sensitivity: 0.5 };
-
-pane.addBinding(params, 'sensitivity', { min: 0, max: 1 }).on('change', (ev) => {
-    window.electronAPI.send('control-change', { param: 'sensitivity', value: ev.value });
-});
