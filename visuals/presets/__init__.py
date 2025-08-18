1df90e429bfaeb3bb06a4c1d2b80420467a4ccf5
ivanvihe <52743025+ivanvihe@users.noreply.github.com>
2025-08-18 22:57:20 +0200

    Refactor particle and intro visuals to Taichi

--- visuals/presets/__init__.py
+++ visuals/presets/__init__.py
@@ -1,4 +1,11 @@
 from .gradient import GradientVisual
 from .abstract_lines import AbstractLinesVisualizer
+from .evolutive_particles import EvolutiveParticlesVisualizer
+from .intro_text_robotica import IntroTextRoboticaVisualizer
 
-__all__ = ["GradientVisual", "AbstractLinesVisualizer"]
+__all__ = [
+    "GradientVisual",
+    "AbstractLinesVisualizer",
+    "EvolutiveParticlesVisualizer",
+    "IntroTextRoboticaVisualizer",
+]
