465edb81b0fa9cbba66be187b5b6bf98bb006ec8
ivanvihe <ivan.vihe@gmail.com>
2025-08-19 20:15:36 +0200

    er

--- test_abstract_lines_visual.py
+++
@@ -1,7 +0,0 @@
-from visuals.presets import AbstractLinesVisualizer
-
-def test_abstract_lines_visual():
-    vis = AbstractLinesVisualizer((40, 8))
-    img = vis.render()
-    assert img.shape == (40, 8)
-    assert img.max() == 1.0 and img.min() == 0.0
